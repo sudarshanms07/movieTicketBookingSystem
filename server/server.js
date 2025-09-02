d4820c765491e7922eb135e4ade50bc38a360118
Sudarshan Maske <ms22maske@gmail.com>
2025-09-02 16:01:54 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import conectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
+import showRouter from './routes/showRoutes.js';
 
 const app = express();
 const port = 3000;
@@ -19,5 +20,5 @@ app.use(clerkMiddleware())
 //API Routes
 app.get('/',(req, res)=> res.send('Server is Live!'))
 app.use('/api/inngest', serve({ client: inngest, functions }))
-
+app.use('/api/show', showRouter)
 app.listen(port, ()=> console.log(`Server listening at http://localhost:${port}`));
